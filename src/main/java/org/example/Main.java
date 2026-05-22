6956d9afce3d6ae37c0a862bcae4c628eefbec7d
UmutGokhanOzdemir <umut.gokhan.ozdemir@gmail.com>
2026-05-22 15:04:02 +0300

    Java OOP'nin Temelleri Gün 2 Projesi Tamamlandı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
-package org.example;
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Lamp lamp = new Lamp(LampType.NORMAL, true, 80);
+        Bed bed = new Bed("Çift Kişilik", 4, 1, 2, 2);
+        Wardrobe wardrobe = new Wardrobe(2, 4, 40);
+        Carpet carpet = new Carpet(3, 5, PaintColor.RED);
+        Ceiling ceiling = new Ceiling(3, PaintColor.RED);
+        Wall wall1 = new Wall("NORTH");
+        Wall wall2 = new Wall("SOUTH");
+        Wall wall3 = new Wall("EAST");
+        Wall wall4 = new Wall("WEST");
+
+        Bedroom bedroom = new Bedroom("My Bedroom", wall1, wall2, wall3, wall4,
+                ceiling, bed, lamp, wardrobe, carpet);
+
+        lamp.turnOn();
+        bed.make();
+        wardrobe.add();
+        carpet.lying();
+        ceiling.create();
+        wall1.create();
+
+        System.out.println("Bedroom name: " + bedroom.getName());
     }
 }
